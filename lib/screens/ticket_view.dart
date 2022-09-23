8a7a86442959b2442efbe19e7fa0283af375c41d
didi2424 <95207941+didi2424@users.noreply.github.com>
2022-09-24 03:50:19 +0700

    Update ticket_view.dart

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -20,10 +20,7 @@ class TicketView extends StatelessWidget {
             Container(
               //decoration BoxDecoration untuk merubah tampilan container
               decoration: BoxDecoration(
-                  color: const Color(0xFF526799),
-                  borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(21),
-                      topRight: Radius.circular(21))),
+                  color: const Color(0xFF526799), borderRadius: const BorderRadius.only(topLeft: Radius.circular(21), topRight: Radius.circular(21))),
               padding: const EdgeInsets.all(16),
               child: Column(
                 children: [
@@ -31,59 +28,61 @@ class TicketView extends StatelessWidget {
                     children: [
                       Text(
                         "NYC",
-                        style:
-                            Styles.headLineStyle3.copyWith(color: Colors.white),
+                        style: Styles.headLineStyle3.copyWith(color: Colors.white),
                       ),
                       // const Spacer(), // memberikan jarak dari ujung ke ujung object
                       Expanded(child: Container()),
                       //ThickContainer(),
-                      ThickContainer(),
-                      Expanded(
-                          child: SizedBox(
-                        height: 24,
-                        child: LayoutBuilder(
-                          builder: (BuildContext context,
-                              BoxConstraints constraints) {
-                            print(
-                                "the width screen is ${constraints.constrainWidth()}"); //untuk menghitung jarak width container/ dan lainnya
-                            return Flex(
-                              direction: Axis.horizontal,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              mainAxisSize: MainAxisSize.max,
-                              // children: List.generate(
-                              //     (constraints.constrainWidth() / 6)
-                              //         .floor(), //menghitung jarak antara dua object oleh constrainWidth kemudian text digenerate sesuai jarak yang dihitung
-                              //     (index) => Text("-")),
 
-                              children: List.generate(
-                                  (constraints.constrainWidth() / 6).floor(),
-                                  (index) => SizedBox(
-                                        width: 3,
-                                        height: 1,
-                                        child: DecoratedBox(
-                                          decoration: BoxDecoration(
-                                              color: Colors.white),
-                                        ),
-                                      )),
-                            );
-                          },
-                        ),
+                      ThickContainer(),
+
+                      Expanded(
+                          child: Stack(
+                        children: [
+                          SizedBox(
+                            height: 24,
+                            child: LayoutBuilder(
+                              builder: (BuildContext context, BoxConstraints constraints) {
+                                print("the width screen is ${constraints.constrainWidth()}"); //untuk menghitung jarak width container/ dan lainnya
+                                return Flex(
+                                  direction: Axis.horizontal,
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  mainAxisSize: MainAxisSize.max,
+                                  // children: List.generate(
+                                  //     (constraints.constrainWidth() / 6)
+                                  //         .floor(), //menghitung jarak antara dua object oleh constrainWidth kemudian text digenerate sesuai jarak yang dihitung
+                                  //     (index) => Text("-")),
+
+                                  children: List.generate(
+                                      (constraints.constrainWidth() / 6).floor(),
+                                      (index) => SizedBox(
+                                            width: 3,
+                                            height: 1,
+                                            child: DecoratedBox(
+                                              decoration: BoxDecoration(color: Colors.white),
+                                            ),
+                                          )),
+                                );
+                              },
+                            ),
+                          ),
+                          Center(
+                            child: Transform.rotate(
+                              angle: 1.5,
+                              child: Icon(
+                                Icons.local_airport_rounded,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
                       )),
-                      Transform.rotate(
-                        angle: 1.5,
-                        child: Icon(
-                          Icons.local_airport_rounded,
-                          color: Colors.white,
-                        ),
-                      ),
 
                       ThickContainer(),
                       Expanded(child: Container()),
                       Text(
                         "JKT",
-                        style: Styles.headLineStyle3.copyWith(
-                            color: Colors
-                                .white), //parameters copyWith pada style mememberikan grant untuk menganti warna
+                        style: Styles.headLineStyle3.copyWith(color: Colors.white), //parameters copyWith pada style mememberikan grant untuk menganti warna
                       )
                     ],
                   )
